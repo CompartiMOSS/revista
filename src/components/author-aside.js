ebabbd470337869dc77a87ccdea29c6e9dc955bb
Alberto Diaz Martin <adiazcan@hotmail.com>
2021-04-05 09:43:16 +0200

    layuot author aside

--- src/components/author-aside.js
+++ src/components/author-aside.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import AuthorPhoto from './author-photo';
 import AuthorLink from './author-link';
+import { makeStyles } from "@material-ui/core"
+
+const useStyles = makeStyles({
+  author: {
+    marginTop: 24,
+    '& img': {
+      objectFit: 'cover',
+      width: 250,
+      height: 250,
+      boxShadow: '0 5px 10px #303030'
+    }
+  }
+})
 
 const AuthorAside = ({ frontmatter, ...props }) => {
+  const classes = useStyles();
+
   return (
-    <div id="divAuthorAside">
-      <div id="divProfilePhoto">
+    <div className={classes.author}>
+      <div>
         <AuthorPhoto author={frontmatter.id} />
       </div>
-      <section id="divSocialInfo">
-        <AuthorLink link={frontmatter.twitter} title="Twitter" id="divTwitter" />
-        <AuthorLink link={frontmatter.blog} title="Blog" id="divBlog" />
+      <section className={classes.links}>
+        <AuthorLink link={frontmatter.twitter} title="Twitter" />
+        <AuthorLink link={frontmatter.blog} title="Blog" />
       </section>
     </div>          
   );
--- src/components/author-page.js
+++ src/components/author-page.js
@@ -2,17 +2,30 @@ import React from 'react';
 import AuthorHeader from './author-header';
 import AuthorAside from './author-aside';
 import ViewArticlesAuthor from './view-articles-author';
+import { Grid } from "@material-ui/core"
 
 const AuthorPage = ({ frontmatter, articles, ...props }) => {
   return (
-    <section id="SectionAuthor">
-      <AuthorAside frontmatter={frontmatter} />      
-      <AuthorHeader frontmatter={frontmatter} />
-      <div id="divAuthorData">
-        <p dangerouslySetInnerHTML={{ __html: frontmatter.content}} />
-        <ViewArticlesAuthor articles={articles} title="Artículos" />
-      </div>
-    </section>
+    <Grid container>
+      <Grid item xs={12} md={2} xl={3}>
+        <AuthorAside frontmatter={frontmatter} />
+      </Grid>
+      <Grid item xs={12} md={10} xl={9}>
+        <AuthorHeader frontmatter={frontmatter} />
+           <div id="divAuthorData">
+            <p dangerouslySetInnerHTML={{ __html: frontmatter.content}} />
+            <ViewArticlesAuthor articles={articles} title="Artículos" />
+          </div>
+      </Grid>
+    </Grid>
+    // <section id="SectionAuthor">
+    //   <AuthorAside frontmatter={frontmatter} />      
+    //   <AuthorHeader frontmatter={frontmatter} />
+    //   <div id="divAuthorData">
+    //     <p dangerouslySetInnerHTML={{ __html: frontmatter.content}} />
+    //     <ViewArticlesAuthor articles={articles} title="Artículos" />
+    //   </div>
+    // </section>
   );
 };
 
