ce934b5b41dafa2c47aa2192316482345ee37749
Alberto Diaz Martin <adiazcan@hotmail.com>
2021-12-08 09:21:31 +0100

    add number 50 to home

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ const HomePage = ({}) => {
   const classes = useStyles();
   const data = useStaticQuery(graphql`{
   current: allMdx(
-    filter: {frontmatter: {magazine: {eq: 49}, type: {eq: "number"}}}
+    filter: {frontmatter: {magazine: {eq: 50}, type: {eq: "number"}}}
   ) {
     edges {
       node {
@@ -42,7 +42,7 @@ const HomePage = ({}) => {
     }
   }
   articles: allMdx(
-    filter: {frontmatter: {magazine: {eq: 49}, type: {ne: "number"}}}
+    filter: {frontmatter: {magazine: {eq: 50}, type: {ne: "number"}}}
   ) {
     edges {
       node {
